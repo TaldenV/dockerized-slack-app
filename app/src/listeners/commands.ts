668513ebd7cd7e7707cb9c5acd86c399d943aeda
TJ VanderLouw <taldenv@gmail.com>
2024-05-14 10:28:40 -0600

    wip: saving for a repo rename

--- app/src/listeners/commands.ts
+++ app/src/listeners/commands.ts
@@ -12,6 +12,16 @@ const register = (app : App) => {
     }
   });
 
+  // health command
+  app.command('/get-gmails', async ({command, ack, respond}) => {
+    try {
+      await ack();
+      await respond(`🤖 I'm alive! 🎉`);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
 };
 
 export default register;
